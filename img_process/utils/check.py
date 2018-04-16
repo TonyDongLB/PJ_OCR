1071db5f3319d90586fe42e99521c359f67396ba
TonyMaster96 <donglibusi@163.com>
2018-04-16 22:34:49 +0800

    对一个图片好用了，还需要改

--- img_process/utils/check.py
+++ img_process/utils/check.py
@@ -31,10 +31,7 @@ def DL_check_rect(src_path,out_path,level='simple'):
     img = cv2.imread(src_path)
 
     img_new = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # img_new = cv2.copyMakeBorder(img_new, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=(255, 255, 255))
     img_new = cv2.copyMakeBorder(img_new, 5, 5, 5, 5, cv2.BORDER_REPLICATE)
-    # logger.info(avg_gray_1,avg_gray_2)
-    # logger.info(param1,param2)
     kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
     # img_new = cv2.morphologyEx(img_new, cv2.MORPH_OPEN, kernel2)
     img_new = cv2.adaptiveThreshold(img_new, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, param1, param2)
@@ -46,46 +43,25 @@ def DL_check_rect(src_path,out_path,level='simple'):
         area = cv2.contourArea(c)
         area_map[area] = c
     sort_keys = sorted(area_map.keys())
-    min_x_y = 10000000
-    max_x_y = -1
-    max_x = -10000000
-    max_y = -10000000
-
-    LT_point = -1
-    RB_point = -1
-    RT_point = -1
-    LB_point = -1
-    area = 0
-    c = None
     lineImg = np.zeros(img_new.shape[:2], dtype=np.uint8)
-    for i in range(-2, 0):
-        area = sort_keys[i]
+    for i in range(-3, 0):
         c = area_map.get(sort_keys[i])
-        for cc in c:
-            # logger.debug(cc)
-            x = cc[0][0]
-            y = cc[0][1]
-            if x + y < min_x_y:
-                min_x_y = x + y
-                LT_point = [x, y]
-            if x + y > max_x_y:
-                max_x_y = x + y
-                RB_point = [x, y]
-            if x - y > max_x:
-                max_x = x - y
-                RT_point = [x, y]
-            if y - x > max_y:
-                max_y = y - x
-                LB_point = [x, y]
-        if i == -2:
-            img_new = cv2.drawContours(img_new, c, -1, (0, 255, 0), 5)
+        if i == -3:
+            img_new = cv2.drawContours(img_new, c, -1, (255, 0, 0), 5)
+            lineImg = cv2.drawContours(lineImg, c, -1, 255, 8)
         else:
-            img_new = cv2.drawContours(img_new, c, -1, (0, 0, 255), 5)
-            lineImg = cv2.drawContours(lineImg, c, -1, 255, 5)
+            if i == -2:
+                img_new = cv2.drawContours(img_new, c, -1, (0, 255, 0), 5)
+                lineImg = cv2.drawContours(lineImg, c, -1, 255, 8)
+            else:
+                img_new = cv2.drawContours(img_new, c, -1, (0, 0, 255), 5)
+                lineImg = cv2.drawContours(lineImg, c, -1, 255, 8)
 
     if os.path.exists(out_path + 'test.jpg'):
         os.remove(out_path + 'test.jpg')
     cv2.imwrite(out_path + 'test.jpg', img_new)
+    cv2.imwrite(out_path + 'lineImg.jpg', lineImg)
+
     # lines = cv2.HoughLines(lineImg, 1, np.pi / 180, int(min(img.shape[0], img.shape[1]) / 2))  # 这里对最后参数使用了经验型的值
     lines = cv2.HoughLinesP(lineImg, 1, np.pi / 360, int(min(img.shape[0], img.shape[1]) / 2))  # 这里对最后参数使用了经验型的值
 
@@ -94,50 +70,14 @@ def DL_check_rect(src_path,out_path,level='simple'):
     newLines_IMG = np.zeros(img_new.shape[:2], dtype=np.uint8)
 
     for (x1, y1, x2, y2) in lines:
+        for x in (x1, x2):
+            if x < 20 or x > lineImg.shape[1] - 20:
+                continue
+        for y in (y1, y2):
+            if y < 20 or y > lineImg.shape[0] - 20:
+                continue
         cv2.line(lineImg, (x1, y1), (x2, y2), (0, 255, 0), 5)
         cv2.line(newLines_IMG, (x1, y1), (x2, y2), 255, 5)
-
-    # # 对距离很近切角度类同的直线进行归一，防止影响结果
-    # newLines = []
-    # while (i < len(lines)):
-    #     line1 = lines[i]
-    #     j = i + 1
-    #     while (j < len(lines)):
-    #         line2 = lines[j]
-    #         if abs(line1[0] - line2[0]) < 10:
-    #             if abs(line1[1] - line2[1]) < np.pi / 4:
-    #                 lines[i][0] = (line1[0] + line2[0]) / 2
-    #                 lines[i][1] = (line1[1] + line2[1]) / 2
-    #                 lines = np.delete(lines, j, 0)
-    #             else:
-    #                 j += 1
-    #         else:
-    #             j += 1
-    #     newLines.append(lines[i])
-    #     i += 1
-    # lines = np.array(newLines)
-    #
-    #
-    # for line in lines:
-    #     rho = line[0]  # 第一个元素是距离rho
-    #     theta = line[1]  # 第二个元素是角度theta
-    #     if (theta < (np.pi / 4.)) or (theta > (3. * np.pi / 4.0)):  # 垂直直线
-    #         # 该直线与第一行的交点
-    #         pt1 = (int(rho / np.cos(theta)), 0)
-    #         # 该直线与最后一行的焦点
-    #         pt2 = (int((rho - lineImg.shape[0] * np.sin(theta)) / np.cos(theta)), lineImg.shape[0])
-    #         # 绘制一条白线
-    #         cv2.line(lineImg, pt1, pt2, (0, 255, 0), 5)
-    #         cv2.line(newLines_IMG, pt1, pt2, 255, 5)
-    #     else:  # 水平直线
-    #         # 该直线与第一列的交点
-    #         pt1 = (0, int(rho / np.sin(theta)))
-    #         # 该直线与最后一列的交点
-    #         pt2 = (lineImg.shape[1], int((rho - lineImg.shape[1] * np.cos(theta)) / np.sin(theta)))
-    #         # 绘制一条直线
-    #         cv2.line(lineImg, pt1, pt2, (0, 255, 0), 5)
-    #         cv2.line(newLines_IMG, pt1, pt2, 255, 5)
-
 
     cv2.imwrite(out_path + 'lineImg.jpg', lineImg)
     cv2.imwrite(out_path + 'newLines_IMG.jpg', newLines_IMG)
@@ -152,14 +92,41 @@ def DL_check_rect(src_path,out_path,level='simple'):
             maxArea = cv2.contourArea(c)
             # img_new = cv2.drawContours(img_new, c, -1, color[i % 3], 8)
             # i += 1
+    min_x_y = 10000000
+    max_x_y = -1
+    max_x = -10000000
+    max_y = -10000000
+
+    LT_point = -1
+    RB_point = -1
+    RT_point = -1
+    LB_point = -1
+    area = cv2.contourArea(maxAreaContour)
+    c = maxAreaContour
+    for cc in c:
+        # logger.debug(cc)
+        x = cc[0][0]
+        y = cc[0][1]
+        if x + y < min_x_y:
+            min_x_y = x + y
+            LT_point = [x, y]
+        if x + y > max_x_y:
+            max_x_y = x + y
+            RB_point = [x, y]
+        if x - y > max_x:
+            max_x = x - y
+            RT_point = [x, y]
+        if y - x > max_y:
+            max_y = y - x
+            LB_point = [x, y]
 
     img_new = cv2.drawContours(img_new, maxAreaContour, -1, (255, 0, 0), 8)
     cv2.imwrite(out_path + 'final.jpg', img_new)
 
 
-    height, width, channels = img.shape
     try:
-
+        height = max(LB_point[1] - LT_point[1], RB_point[1] - RT_point[1])
+        width = max(RT_point[0] - LT_point[0], RB_point[0] - LB_point[0])
         pts1 = np.float32([LT_point, RT_point, LB_point, RB_point])
         pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
         # 仿射变换
@@ -171,91 +138,15 @@ def DL_check_rect(src_path,out_path,level='simple'):
     #    pass
     finally:
         pass
+    cv2.imwrite(out_path + '_____getPerspectiveTransform.jpg', img)
     result = {}
     result["area"] = (LB_point[1] - LT_point[1]) * (RB_point[0] - LB_point[0])
     result["all_area"] = height * width
     result["img"] = img
     result["img_new"] = img_new
     result["c"] = c
-    # print(result["area"],result["all_area"],area/(height * width))
+    print(result["area"],result["all_area"],area/(height * width))
     return result
-    #
-    # if os.path.exists(out_path):
-    #     os.remove(out_path)
-    # result = None
-    # out_dir_path = os.path.dirname(out_path)
-    # if not os.path.exists(out_dir_path):
-    #     os.makedirs(out_dir_path)
-    #
-    # img = cv2.imread(src_path)
-    # img = cv2.pyrMeanShiftFiltering(img, 25, 10)
-    # cv2.imwrite(out_path + 'pyrMeanShiftFiltering.jpg', img)
-    #
-    #
-    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #
-    # cv2.imwrite(out_path + 'gray.jpg', img_gray)
-    # # img_new = cv2.copyMakeBorder(img_new, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=(255, 255, 255))
-    # img_gray = cv2.copyMakeBorder(img_gray, 5, 5, 5, 5, cv2.BORDER_REPLICATE)
-    # # logger.info(avg_gray_1,avg_gray_2)
-    # # logger.info(param1,param2)
-    # kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-    # # img_new = cv2.morphologyEx(img_new, cv2.MORPH_OPEN, kernel2)
-    # param1 = default_params["11_3"]["param"][0]
-    # param2 = default_params["11_3"]["param"][1]
-    # img_thresh = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, param1, param2)
-    # cv2.imwrite(out_path + 'adaptiveThreshold.jpg', img_thresh)
-    #
-    # # img_new = cv2.medianBlur(img_new, 3)
-    # img_thresh = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel2)
-    # edges = cv2.Canny(img_thresh, 50, 255)
-    #
-    # tempIamge  = img_gray.copy()
-    # lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 5, 50)  # 这里对最后参数使用了经验型的值
-    # tempIamge = cv2.cvtColor(tempIamge, cv2.COLOR_GRAY2RGB)
-    # for x1, y1, x2, y2 in lines[0]:
-    #     cv2.line(tempIamge, (x1, y1), (x2, y2), (0, 255, 0), 5)
-    # # for line in lines[0]:
-    # #     rho = line[0]  # 第一个元素是距离rho
-    # #     theta = line[1]  # 第二个元素是角度theta
-    # #     if (theta < (np.pi / 4.)) or (theta > (3. * np.pi / 4.0)):  # 垂直直线
-    # #         # 该直线与第一行的交点
-    # #         pt1 = (int(rho / np.cos(theta)), 0)
-    # #         # 该直线与最后一行的焦点
-    # #         pt2 = (int((rho - tempIamge.shape[0] * np.sin(theta)) / np.cos(theta)), tempIamge.shape[0])
-    # #         # 绘制一条白线
-    # #         cv2.line(tempIamge, pt1, pt2, (0, 255, 0), 5)
-    # #     else:  # 水平直线
-    # #         # 该直线与第一列的交点
-    # #         pt1 = (0, int(rho / np.sin(theta)))
-    # #         # 该直线与最后一列的交点
-    # #         pt2 = (tempIamge.shape[1], int((rho - tempIamge.shape[1] * np.cos(theta)) / np.sin(theta)))
-    # #         # 绘制一条直线
-    # #         cv2.line(tempIamge, pt1, pt2, (0, 255, 0), 5)
-    # cv2.imwrite(out_path+'!!!Canny.jpg', edges)
-    # cv2.imwrite(out_path+'!!!HoughLinesP.jpg', tempIamge)
-    #
-    #
-    #
-    # image, cnts, hierarchy = cv2.findContours(img_thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    # img_new = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
-    # maxArea = 0
-    # test = img_new.copy()
-    # for i in range(len(cnts)):
-    #     area = cv2.contourArea(cnts[i])
-    #     if area > maxArea:
-    #         maxArea = area
-    #         final_CNT = i
-    #         cv2.drawContours(test, cnts, i, (0, 255, 0), 5)
-    # img_new = cv2.drawContours(img_new, cnts, final_CNT, (0, 0, 255), -1)
-    # if os.path.exists(out_path+'test.jpg'):
-    #     os.remove(out_path+'test.jpg')
-    # cv2.imwrite(out_path+'!!!test.jpg', img_new)
-    # cv2.imwrite(out_path+'!!!edges.jpg', test)
-    #
-    #
-    #
-    # pass
 
 
 def check_rect(src_path,out_path,level='simple'):
